5bd472db8ccea40bcb14ecc32a81489706eb2597
1678 <a1678@MacBook-Air-Artem.local>
2024-05-17 19:59:07 +0300

    headerWrap

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,14 +1,18 @@
 mainObj.header =
 {
+	headerWrap:document.querySelector('._header__wrap'),
+
 	show(instance)
 	{
 		if(instance.classList.contains('active'))
 		{
 			instance.classList.remove('active')
+			this.headerWrap.classList.remove('active')
 		}
 		else
 		{
 			instance.classList.add('active')
+			this.headerWrap.classList.add('active')
 		}
 	}
 }
