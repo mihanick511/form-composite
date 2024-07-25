fcb797259e0cf66a11bdfeb45630e6792403c4e1
mihanick511 <sautievmihail@gmail.com>
2024-07-25 18:30:02 +0300

    newsHazard

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -8,5 +8,21 @@ let mainObj =
 	inition()
 	{
 		this.production.initSlider()
+		this.actionResise()
+	},
+	actionClass(tags, className, action)
+	{
+		tags.forEach((el)=>
+		{
+			action === 'add' ?
+			el.classList.add(className):
+			el.classList.remove(className)
+		})
+	},
+	actionResise()
+	{
+		window.addEventListener('resize', () => {
+  			this.news.move();
+		});
 	}
 }
--- src/components/news/news.js
+++ src/components/news/news.js
@@ -1,14 +1,31 @@
-mainObj.news = // в главном обьекте создаем обьект news
+mainObj.news =
 {
-	item: document.querySelectorAll('._item'),// находим все div item(это массив)
-	move()//в обьекте news создаем метод move
+	item: document.querySelectorAll('._item'),
+	// перенесли переменную heig в метод move
+	move()
 	{
-		for(let i = 1; i < this.item.length; i++)//в move с помощью for проходим по массиву item
+		let heig = document.querySelectorAll('._heig')
+		//if(this.item[0].querySelectorAll('.news__item-el').lenght > 1) return;
+		if(window.innerWidth <= 1100)
 		{
-			let first = this.item[i].querySelectorAll('._wr')[0]//для выделения нужных нам тегов создаем массив first из 0вых элементов массива item
-			this.item[i-1].innerHTML += first.innerHTML//оброщаемся к 0му элементу массива item и к 1му с помощью смещения и перекладываем в него внутряк элементов first
-			first.remove()//удаляем ненужные нам блоки из массива first находящиеся в исходных блоках
+			mainObj.actionClass(heig, 'heig', 'remove');
+			for(let i = 1; i < this.item.length; i++)
+			{
+				let first = this.item[i].querySelector('._wr')
+				this.item[i-1].innerHTML += first.outerHTML
+				first.remove();
+			}
+		}
+		if(window.innerWidth > 1100)
+		{
+			mainObj.actionClass(heig, 'heig', 'add'); // использовали нашу переменную heig вместо document.querySelectorAll('_heig')
+			for(let i = 0; i < this.item.length; i++)
+			{
+				if(!this.item[i+1]) return;
+				let last = this.item[i].querySelectorAll('._wr')[1]
+				this.item[i+1].innerHTML += last.outerHTML
+				last.remove();
+			}
 		}
 	}	
 }
-//баг: кнопка срабатывает повторно, return и break не работают
